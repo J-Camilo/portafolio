35e4eae4066930f6b2721e77db85a85dd20ff3a7
Juan.fong <juan.fong@bitsoluciones.com.co>
2025-04-13 15:33:06 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Threads from "./components/backgrounAnimate/threads"
+
+function App() {
+
+  return (
+    <>
+
+      <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+        <Threads
+          amplitude={2}
+        />
+      </div>
+    </>
+  )
+}
+
+export default App
