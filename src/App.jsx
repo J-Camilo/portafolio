d7987a4fc3d371214c31d2b5ed6cd9c93acca669
Juan.fong <juan.fong@bitsoluciones.com.co>
2025-04-13 15:56:15 -0500

    se agrega el fondo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Threads from "./components/backgrounAnimate/threads"
 
 function App() {
 
   return (
     <>
+      <div style={{ width: '100%', height: '600px', position: 'absolute' }}>
+        <Threads
+          amplitude={2} 
+        />
+      </div>
     </>
   )
 }
