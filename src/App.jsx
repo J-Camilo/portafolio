a5af40ca564c7cd5f479ba4d523da33d936080fc
Juan.fong <juan.fong@bitsoluciones.com.co>
2025-04-13 16:09:33 -0500

    se agrega otra ruta de el fondo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import Threads from "./components/backgrounAnimate/threads"
+import Threads from "../src/components/backgrounAnimate/threads"
 
 function App() {
-
   return (
     <>
       <div style={{ width: '100%', height: '600px', position: 'absolute' }}>
