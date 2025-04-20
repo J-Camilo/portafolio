6f979ddb51c37c059af43e0a865cf7cc6e1c5dc6
Juan.fong <juan.fong@bitsoluciones.com.co>
2025-04-19 21:00:40 -0500

    se agrega home

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import Threads from "../src/components/backgrounAnimate/threads"
+import Home from "./layout/home/home"
 
 function App() {
   return (
-    <>
-      <div style={{ width: '100%', height: '600px', position: 'absolute' }}>
-        <Threads
-          amplitude={2} 
-        />
-      </div>
-    </>
+ <>
+ <Home />
+ </>
   )
 }
 
---
+++ src/layout/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>home</div>
+
+//     <div style={{ width: '100%', height: '600px', position: 'absolute' }}>
+//     <Threads
+//       amplitude={2} 
+//     />
+//   </div>
+  )
+}
+
+export default Home
